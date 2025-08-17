d2a0ab8b623be4f70bf7686886cc232169ebb892
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2025-08-17 00:19:50 -0600

    First Commit

---
+++ coding_interview_questions/src/main/java/com/interview/questions/LeetCode217.java
@@ -0,0 +1,23 @@
+package com.interview.questions;
+
+import java.util.HashSet;
+
+public class LeetCode217 {
+    public static void main(String[] args) {
+        // Contains Duplicate
+        int array[] = {1,1,1,3,3,4,3,2,4,2};
+        boolean response = containsDuplicates(array);
+        System.out.println(response);
+    }
+
+    private static boolean containsDuplicates(int array[]){
+        HashSet<Integer> hashSet = new HashSet<>();
+        for(int value: array){
+            if(hashSet.contains(value)){
+                return true;
+            }
+            hashSet.add(value);
+        }
+        return false;
+    }
+}
